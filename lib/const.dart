947ef81f3c52305d56a799e8b2b1ac15a90c81de
Ayush Kasera <ayushkaserapk@gmail.com>
2022-06-13 19:58:59 +0530

    bottom Nav Screens

--- lib/const.dart
+++ lib/const.dart
@@ -1,7 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce_app/views/screens/cart_screen.dart';
+import 'package:ecommerce_app/views/screens/feeds_screen.dart';
+import 'package:ecommerce_app/views/screens/home_screen.dart';
+import 'package:ecommerce_app/views/screens/profile_screen.dart';
+import 'package:ecommerce_app/views/screens/search_screen.dart';
+import 'package:ecommerce_app/views/screens/upload_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+
+//pages
+
+List pages = [
+  HomeScreen(),
+  FeedScreen(),
+  SearchScreen(),
+  CartScreen(),
+  UploadScreen(),
+  ProfileScreen(),
+];
 
 var backgroundColor = Colors.black;
 
--- lib/views/screens/auth/bottom_navbar.dart
+++ lib/views/screens/auth/bottom_navbar.dart
@@ -1,15 +1,70 @@
+import 'package:ecommerce_app/const.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class BottomNavBar extends StatefulWidget {
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
 
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({Key? key}) : super(key: key);
+class _BottomNavBarState extends State<BottomNavBar> {
+  int pageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Welcome'),
-      ),
+      bottomNavigationBar: CupertinoTabBar(
+          onTap: (index) {
+            setState(() {
+              pageIndex = index;
+            });
+          },
+          currentIndex: pageIndex,
+          activeColor: Colors.white,
+          // inactiveColor: Colors.white,
+          backgroundColor: backgroundColor,
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: backgroundColor,
+              icon: Icon(
+                Icons.home,
+                size: 30,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.rss_feed,
+                  size: 30,
+                ),
+                label: 'Feed'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  size: 30,
+                ),
+                label: 'Search'),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.shopping_bag,
+                size: 30,
+              ),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.upload,
+                  size: 30,
+                ),
+                label: 'Upload'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  size: 30,
+                ),
+                label: 'Person'),
+          ]),
+      body: pages[pageIndex],
     );
   }
 }
